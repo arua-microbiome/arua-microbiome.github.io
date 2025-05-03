38e0f9ef4e112bb171cd9c48b4f18acd2526b264
George Kalogiannis <g.kalogiannis23@imperial.ac.uk>
2025-05-03 15:16:00 +0100

    updated config

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "Amplicon analysis with QIIME2",
+        "excerpt":"By Adam Rivers, Designed from the official QIIME2 tutorials On this page Data sets Connecting to Ceres Set up your working directory Understanding QIIME2 files Import your paired-end sequences Examine the quality of the data Selecting Sequence Variants Option 1: Dada2 (Slower) Option 2: Deblur (Faster) Adding metadata and examining...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/https://arua-microbiome.github.io/tutorials/qiime2/",
+        "teaser":null},{
+        "title": "Software to install before the microbiome workshop",
+        "excerpt":"On this page Windows Mac Linux Most of the tutorials that will be done at the microbiome workshop will be done remotely on Ceres, the high-performance computer, but some basic software is needed to connect and visualize the data we will be generating. All of these programs should be installable...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/https://arua-microbiome.github.io/tutorials/software/",
+        "teaser":null}]
